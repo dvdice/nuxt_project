e33bc97d7a805316b1de36b493174aade246a549
dvdice <valitovdamir73@gmail.com>
2025-03-20 23:29:05 +0400

    Использование встроенного компонента NuxtPage, который управляет маршрутизацией с помощью VueRouter (NuxtPage - обертка над RouterView). Создание директивы pages. Путь до компонентов = URL страницы

---
+++ nuxt_project/pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+    <h2>About</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu lectus efficitur, fermentum turpis fermentum,
+        dapibus elit. Curabitur ac justo dui. Nulla nec ultrices nibh, sed tincidunt metus. Nam suscipit eget leo at consequat.
+        Maecenas in placerat mauris. Duis efficitur non leo nec congue. In iaculis est odio, ut efficitur dolor vestibulum in.
+        Etiam lectus nunc, laoreet at lacus vel, lobortis porta magna.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu lectus efficitur, fermentum turpis fermentum,
+        dapibus elit. Curabitur ac justo dui. Nulla nec ultrices nibh, sed tincidunt metus. Nam suscipit eget leo at consequat.
+        Maecenas in placerat mauris. Duis efficitur non leo nec congue. In iaculis est odio, ut efficitur dolor vestibulum in.
+        Etiam lectus nunc, laoreet at lacus vel, lobortis porta magna.
+    </p>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
