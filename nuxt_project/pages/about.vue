8b66e8b88a0e80fa84af451010fdef83d20be210
dvdice <valitovdamir73@gmail.com>
2025-03-21 00:02:28 +0400

    Использование NuxtLink: По сути это обычный тэг a, но с приколами. Во первых он не делает запрос на получение страницы. Вообще никаких запросов не делает просто переключает компонент. Во вторых активной ссылке задает классы router-link-active, router-link-exact-active которые в последствии можно стилизовать

--- nuxt_project/pages/about.vue
+++ nuxt_project/pages/about.vue
@@ -1,17 +1,30 @@
 <template>
-    <h2>About</h2>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu lectus efficitur, fermentum turpis fermentum,
-        dapibus elit. Curabitur ac justo dui. Nulla nec ultrices nibh, sed tincidunt metus. Nam suscipit eget leo at consequat.
-        Maecenas in placerat mauris. Duis efficitur non leo nec congue. In iaculis est odio, ut efficitur dolor vestibulum in.
-        Etiam lectus nunc, laoreet at lacus vel, lobortis porta magna.
-    </p>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu lectus efficitur, fermentum turpis fermentum,
-        dapibus elit. Curabitur ac justo dui. Nulla nec ultrices nibh, sed tincidunt metus. Nam suscipit eget leo at consequat.
-        Maecenas in placerat mauris. Duis efficitur non leo nec congue. In iaculis est odio, ut efficitur dolor vestibulum in.
-        Etiam lectus nunc, laoreet at lacus vel, lobortis porta magna.
-    </p>
+    <div>
+        <header>
+            <NuxtLink to="/">Nuxt Project</NuxtLink>
+            <nav>
+                <ul>
+                    <li><NuxtLink to="/Home">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+
+        <h2>About</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu lectus efficitur, fermentum turpis fermentum,
+            dapibus elit. Curabitur ac justo dui. Nulla nec ultrices nibh, sed tincidunt metus. Nam suscipit eget leo at consequat.
+            Maecenas in placerat mauris. Duis efficitur non leo nec congue. In iaculis est odio, ut efficitur dolor vestibulum in.
+            Etiam lectus nunc, laoreet at lacus vel, lobortis porta magna.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu lectus efficitur, fermentum turpis fermentum,
+            dapibus elit. Curabitur ac justo dui. Nulla nec ultrices nibh, sed tincidunt metus. Nam suscipit eget leo at consequat.
+            Maecenas in placerat mauris. Duis efficitur non leo nec congue. In iaculis est odio, ut efficitur dolor vestibulum in.
+            Etiam lectus nunc, laoreet at lacus vel, lobortis porta magna.
+        </p>
+    </div>
 </template>
 
 <script setup lang="ts">
