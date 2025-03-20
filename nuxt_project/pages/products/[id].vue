7939435476768079bbb2f924286e36320175008f
dvdice <valitovdamir73@gmail.com>
2025-03-20 23:40:00 +0400

    Если задать имя компонента в фигурных скобках, то Nuxt будет воспринимать его как параметр. То есть при переходе на /products/123 nuxt поймет, что такого компонента нет и выдаст этот ([id].vue). Для получения парметров URL используется метод useRoute из VueRouter

---
+++ nuxt_project/pages/products/[id].vue
@@ -0,0 +1,20 @@
+<template>
+    <h2>Product</h2>
+    <p>
+        Product details for {{ id }}
+    </p>
+</template>
+
+<script setup lang="ts">
+const { id } = useRoute().params;
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
